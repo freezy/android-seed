2cb8cc936ec50cc03e37cac4a915ddf23a3e6ac0
freezy <freezy@xbmc.org>
2014-12-12 12:54:26 +0100

    Added Lollipop style toolbar and navigation drawer.

--- app/src/main/java/com/example/android/app/MainActivity.java
+++ app/src/main/java/com/example/android/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.app;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,16 +15,54 @@ import android.view.ViewGroup;
 
 public class MainActivity extends ActionBarActivity {
 
+
+	private Toolbar toolbar;
+	private DrawerLayout drawerLayout;
+	private ActionBarDrawerToggle drawerToggle;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		if (toolbar != null) {
+			toolbar.setTitle(R.string.app_name);
+			setSupportActionBar(toolbar);
+		}
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+			}
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+			}
+		};
+		drawerLayout.setDrawerListener(drawerToggle);
+
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment())
 					.commit();
 		}
 	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
